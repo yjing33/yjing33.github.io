8962af6aed355b50558156396a59ce169c7e1f97
yjing33 <yjing33@users.noreply.github.com>
2026-05-11 05:48:34 +0000

    Deploying to gh-pages from @ yjing33/yjing33.github.io@98af2a05bce96a33629530293678e3bea36ad472 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yjing33/jingyu.github.io",
+    "data-repo": "yjing33/yjing33.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
